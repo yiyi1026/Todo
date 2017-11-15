d87807824f4e3425e6cb206c1f127d1d9d6bc9b5
yiyi1026 <vivian.li.uibe@gmail.com>
2017-11-14 20:47:12 -0800

    modify user model

--- db/migrate/20171115044424_create_users.rb
+++ db/migrate/20171115044424_create_users.rb
@@ -1,7 +1,6 @@
-class CreateApiUsers < ActiveRecord::Migration[5.0]
+class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :username, null: false
       t.string :username, null: false
       t.string :email, null: false
       t.string :description
@@ -11,6 +10,7 @@ class CreateApiUsers < ActiveRecord::Migration[5.0]
 
       t.timestamps
     end
+
     add_index :users, :email, unique: true
     add_index :users, :session_token, unique: true
   end
