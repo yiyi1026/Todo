8e49c432e8bb0bfbc62df6e0f1880c90aa904863
yiyi1026 <vivian.li.uibe@gmail.com>
2017-11-14 20:24:16 -0800

    add user models

--- Gemfile
+++ Gemfile
@@ -51,6 +51,7 @@ group :development do
   gem 'pry-rails'
   gem 'annotate'
   
+  
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
+# == Route Map
+#
+#      Prefix Verb   URI Pattern            Controller#Action
+#    api_user POST   /api/user(.:format)    api/users#create {:format=>:json}
+# api_session GET    /api/session(.:format) api/sessions#show {:format=>:json}
+#             DELETE /api/session(.:format) api/sessions#destroy {:format=>:json}
+#             POST   /api/session(.:format) api/sessions#create {:format=>:json}
+#        root GET    /                      static_pages#root
+# 
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
